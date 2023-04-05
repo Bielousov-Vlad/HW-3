1d1e113eeba14c56547a64d186fde92c7f004db9
Bielousov-Vlad <64091216+Bielousov-Vlad@users.noreply.github.com>
2023-04-06 02:00:07 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Введіть перше число: "));
+let num2 = Number(prompt("Введіть друге число: "));
+
+let sum  = num1 + num2;
+let  minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+alert(num1 + "+" + num2 + "=" + sum + "\n" +
+      num1 + "-" + num2 + "=" +  minus + "\n" +
+      num1 + "*" + num2 + "=" + multiply + "\n" +
+      num1 + "/" + num2 + "=" + divide);
